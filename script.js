7dd85a87b8bb55c2ede289abcc16c855876fba8e
Bermet <oceaninapuddle@gmail.com>
2023-11-14 23:55:29 +0600

    add NEW ARRIVALS

--- script.js
+++ script.js
@@ -37,5 +37,30 @@ tabs.forEach((tab) => {
     });
     tab.classList.add("active_tab");
   });
-  
+});
+
+// Дать детям на самостоятельную
+
+var swiperProducts = new Swiper(".new_container", {
+  spaceBetween: 24,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1400: {
+      slidesPerView: 4,
+      spaceBetween: 24,
+    },
+  },
 });
