d1d654a6df322fbb5d9975fec571874f2ab5dbc9
Bermet <oceaninapuddle@gmail.com>
2023-11-14 22:24:18 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+var swiperCategories = new Swiper(".categories_container", {
+  spaceBetween: 24,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1400: {
+      slidesPerView: 6,
+      spaceBetween: 24,
+    },
+  },
+});
+
+const tabs = document.querySelectorAll("[data-target]");
+const tabContent = document.querySelectorAll("[content]");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.target);
+    tabContent.forEach((tabContent) => {
+      tabContent.classList.remove("active_tab");
+    });
+    target.classList.add("active_tab");
+    tabs.forEach((tab) => {
+      tab.classList.remove("active_tab");
+    });
+    tab.classList.add("active_tab");
+  });
+  
+});
